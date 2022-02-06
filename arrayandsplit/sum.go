fdddeee71c782ff3ff64d5dd4b66f43a8059452c
xiahaohai123 <xiahaohai@qq.com>
2022-02-06 18:08:27 +0800

    迭代法

--- arrayandsplit/sum.go
+++ arrayandsplit/sum.go
@@ -2,8 +2,8 @@ package arrayandsplit
 
 func Sum(numbers [5]int) (sum int) {
 	sum = 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return
 }
