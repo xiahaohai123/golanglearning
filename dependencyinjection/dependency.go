dce9be21e459a0d7041d00bf8711781ae227211e
summer_sea <xiahaohai@qq.com>
2022-02-20 10:06:39 +0800

    别名

--- dependencyinjection/dependency.go
+++ dependencyinjection/dependency.go
@@ -14,6 +14,12 @@ func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "world")
 }
 
+type AAA func(w http.ResponseWriter, r *http.Request)
+
+func (receiver AAA) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	_ = http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler))
+	_ = http.ListenAndServe(":5000", AAA(MyGreeterHandler))
 }
